496e5fb2033e086532e28871847374339e0417c5
空空如也 <shift_1220@163.com>
2019-12-28 09:20:47 +0800

    提交文件

--- api/tests_unittest.py
+++ api/tests_unittest.py
@@ -10,28 +10,56 @@ class UserTest(unittest.TestCase):
         self.auth = ('admin', 'lcfwku1220')
 
     def test_get_user(self):
-        r = requests.get(self.base_url + '1', auth=self.auth)
+        r = requests.get(self.base_url + '2', auth=self.auth)
         result = r.json()
-        self.assertEqual(result['username'], 'admin')
-        self.assertEqual(result['email'], 'admin@admin.com')
+        self.assertEqual(result['username'], 'test1')
+        self.assertEqual(result['email'], 'test1@admin.comm')
 
     def test_add_user(self):
-        data = {'username': 'test5', 'email': 'test3@admin.com', 'groups': 'http://127.0.0.1:8000/groups/1/'}
+        data = {'username': 'test6', 'email': 'test6@admin.com', 'groups': 'http://127.0.0.1:8000/groups/4/'}
         r = requests.post(self.base_url, data=data, auth=self.auth)
         result = r.json()
         print(result)
-        self.assertEqual(result['username'], 'test5')
+        self.assertEqual(result['username'], 'test6')
 
     def test_update_user(self):
-        data = {'email': 'test2@admin.com'}
+        data = {'email': 'test3@admin.com'}
         r = requests.patch(self.base_url + '3', data=data, auth=self.auth)
         result = r.json()
-        self.assertEqual(result['email'], 'est2@admin.com')
+        self.assertEqual(result['email'], 'test2@admin.com')
 
     def test_auth(self):
         r = requests.get(self.base_url)
         result = r.json()
         self.assertEqual(result['detail'], '身份认证信息未提供。')
+
+
+class GroupTest(unittest.TestCase):
+
+    def setUp(self):
+        self.base_url = 'http://127.0.0.1:8000/groups/'
+        self.auth = ('admin', 'lcfwku1220')
+
+    def test_group_deve(self):
+        r = requests.get(self.base_url + '2', auth=self.auth)
+        result = r.json()
+        self.assertEqual(result['name'], '管理员组')
+
+    def test_add_group(self):
+        data = {'name': '产品组'}
+        r = requests.post(self.base_url, auth=self.auth, data=data)
+        result = r.json()
+        self.assertEqual(result['name'], '产品组')
+
+    def test_update_group(self):
+        data = {'name': 'Boss'}
+        r = requests.patch(self.base_url + '2', auth=self.auth, data=data)
+        result = r.json()
+        self.assertEqual(result['name'], 'Boss')
+
+    def test_delete_group(self):
+        r = requests.delete(self.base_url + '1', auth=self.auth)
+        self.assertEqual(r.status_code, 204)
 
 
 if __name__ == '__main__':
