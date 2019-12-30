249dbd5e8e01d5c3806003e7d2a765d962e0dc66
Liuchangfu <shift_1220@163.com>
2019-12-31 00:10:27 +0800

    提交文件

--- api/test_project/run.py
+++ api/test_project/run.py
@@ -1,10 +1,12 @@
 # _*_ coding:utf-8 _*_
 import os
 import sys
-dir = os.path.join(os.path.dirname(os.path.dirname(__file__)))
-sys.path.append(dir)
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+print(rootPath)
+dir = os.path.split(rootPath)[0]
 print(dir)
-print(sys.path)
+sys.path.append(os.path.split(rootPath)[0])
 import datetime
 import unittest
 from loguru import logger
@@ -18,19 +20,22 @@ db = DB()
 db.init_data()
 
 # 测试用例文件夹
-testDir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'test_project', 'testDir')
+# testDir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'test_project', 'testDir')
+testDir = dir + '//api' + '//test_project' + '//testDir'
 logger.info(testDir)
 
 # 测试报告文件夹
-report = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'test_project', 'report')
+# report = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'test_project', 'report/')
+report = dir + '//api' + '//test_project' + '//report'
 logger.info(report)
 
 # 加载测试用例
-discover = unittest.defaultTestLoader.discover(testDir, pattern='test*.py')
+# discover = unittest.defaultTestLoader.discover(testDir, pattern='test*.py')
+discover = unittest.TestLoader().discover("testDir")
 
 now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 # 测试报告名字
-report_name = report + '/' + now + '_test_report.html'
+report_name = report + now + '_test_report.html'
 logger.info(report_name)
 
 with open(report_name, 'wb') as f:
